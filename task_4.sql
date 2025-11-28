ed10af6acaacb93e346084a2b0baed72457f92ad
Aisha-Amru-Mudesir <139975048+aish-mudesir@users.noreply.github.com>
2025-11-28 10:10:29 +0300

    Create task_4.sql

---
+++ task_4.sql
@@ -0,0 +1,15 @@
+-- Prints the full description of the table `books`
+-- Without using DESCRIBE or EXPLAIN
+
+SELECT 
+    COLUMN_NAME,
+    COLUMN_TYPE,
+    IS_NULLABLE,
+    COLUMN_KEY,
+    COLUMN_DEFAULT,
+    EXTRA
+FROM 
+    INFORMATION_SCHEMA.COLUMNS
+WHERE 
+    TABLE_SCHEMA = 'alx_book_store'
+    AND TABLE_NAME = 'books';
